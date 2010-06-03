5eb11943aba3f04d4f423716dd6d918115f295a1
jmstacey <jmstacey@f69abe3c-2ee2-4550-9a6d-17bd458115be>
2010-06-03 06:24:25 +0000

    Added main Stream Wrapper API plugin header file.
    git-svn-id: https://gsoc.svn.wordpress.org/2010/jmstacey/stream-wrapper-api/trunk@113 f69abe3c-2ee2-4550-9a6d-17bd458115be

---
+++ stream-wrapper-api/stream-wrapper-api.php
@@ -0,0 +1,15 @@
+<?php
+/*
+Plugin Name: Stream Wrapper API
+Plugin URI: TBD
+Description: Stream Wrapper API provides the core foundation upon which wrappers for WordPress are built.
+Version: 1.0
+Author: Jon Stacey
+Author URI: http://jonsview.com
+*/
+
+define('STREAM_WRAPPER_API_VERSION', '1.0');
+
+// class stuff
+
+?>
