b3ba5fe0cfc55aefc16f0373ca0d531250ffbca3
jmstacey <jmstacey@f69abe3c-2ee2-4550-9a6d-17bd458115be>
2010-06-16 23:28:20 +0000

    Initial import of wp-stream-wrapper-local.php
    git-svn-id: https://gsoc.svn.wordpress.org/2010/jmstacey/wp-stream-wrappers/trunk@333 f69abe3c-2ee2-4550-9a6d-17bd458115be

---
+++ wp-stream-wrapper-local.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * This file contains the wp-content WordPress Stream Wrapper which implements
+ * the "local://" scheme. This is a simple and complete stream wrapper
+ * implementation for testing, reference, and extension purposes. Other stream
+ * wrappers that manipulate files on the local filesystem can quickly extend
+ * this class to suit more specific needs. The WP Test [test://] stream
+ * is an excellent example of this use case.
+ *
+ * @package Stream Wrappers
+ */
+
+?>
