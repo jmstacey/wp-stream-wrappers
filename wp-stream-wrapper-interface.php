aec5840e67e9bc8e7d46ea13a79b419e1e093bdd
jmstacey <jmstacey@f69abe3c-2ee2-4550-9a6d-17bd458115be>
2010-06-16 23:27:59 +0000

    Added Php_Stream_Wrapper_Interface.
    git-svn-id: https://gsoc.svn.wordpress.org/2010/jmstacey/wp-stream-wrappers/trunk@330 f69abe3c-2ee2-4550-9a6d-17bd458115be

--- wp-stream-wrapper-interface.php
+++ wp-stream-wrapper-interface.php
@@ -8,7 +8,7 @@
  * stream could be a local file on the hard drive, a web page, or even
  * input from STDIN.
  *
- * IMPORTANT: PHP 5 fopen() only supports URIs in the form of
+ * Note: PHP 5 fopen() only supports URIs in the form of
  * "scheme://target" despite the fact that according to RFC 3986 a URI's
  * scheme component delimiter is in general just ":", and not "://".
  * As a result of this PHP limitation and for consitency WordPress will
@@ -22,5 +22,50 @@
  */
 
 
+/**
+ * Generic PHP stream wrapper interface
+ *
+ * A simple prototype of the stream wrapper class used by the WordPress
+ * stream wrapper interface. Refer to WP_Stream_Wrapper_Interface for
+ * further documentation on resources and methods.
+ *
+ * Note: This interface should not be directly implemented. Instead, extend
+ * WP_Stream_Wrapper_Interface.
+ *
+ * @package    Stream Wrappers
+ * @author     Jon Stacey <jon@jonsview.com>
+ * @version    Release: 1.0.0
+ * @link       http://www.php.net/manual/en/class.streamwrapper.php
+ * @see        WP_Stream_Wrapper_Interface
+ * @since      Class available since Release 1.0.0
+ */
+interface Php_Stream_Wrapper_Interface {	
+	public function __construct();
+	
+	public function dir_closedir();	
+	public function dir_opendir($uri, $options);
+	public function dir_readdir();
+	public function dir_rewinddir();
+	
+	public function mkdir($uri, $mode, $options);
+	public function rename($from_uri, $to_uri);
+	public function rmdir($uri, $options);
+	
+	public function stream_cast($cast_as);
+	public function stream_close();
+	public function stream_eof();
+	public function stream_flush();
+	public function stream_lock($operation);
+	public function stream_open($uri, $mode, $options, &$opened_uri);
+	public function stream_read($count);
+	public function stream_seek($offset, $whence);
+	public function stream_set_option($option, $arg1, $arg2);
+	public function stream_stat();
+	public function stream_tell();
+	public function stream_write($data);
+	public function unlink($uri);
+	
+	public function url_stat($uri, $flags);
+}
 
 ?>
