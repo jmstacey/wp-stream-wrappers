5d75fa5b38e30cd822a1c466638bf1edb910df70
Jon Stacey <jon@jonsview.com>
2010-06-17 15:13:06 -0500

    Implemented WP_Stream_Wrapper_Interface::rename() and made some other spacing adjustments to mkdir().

--- wp-local-stream-wrapper-base.php
+++ wp-local-stream-wrapper-base.php
@@ -238,14 +238,36 @@ abstract class WP_Local_Stream_Wrapper_Base implements WP_Stream_Wrapper_Interfa
 	 */
 	public function mkdir($uri, $mode, $options) {
 		$this->uri = $uri;
-	    $recursive = (bool)($options & STREAM_MKDIR_RECURSIVE);
-	    if ($options & STREAM_REPORT_ERRORS) {
-	      return mkdir($this->get_local_path(), $mode, $recursive);
-	    }
-	    else {
-	      return @mkdir($this->get_local_path(), $mode, $recursive);
-	    }
+		$recursive = (bool)($options & STREAM_MKDIR_RECURSIVE);
+		if ($options & STREAM_REPORT_ERRORS) {
+			return mkdir($this->get_local_path(), $mode, $recursive);
+		}
+		else {
+			return @mkdir($this->get_local_path(), $mode, $recursive);
+		}
 	}
+
+	/**
+	 * Implements WP_Stream_Wrapper_Interface::rename()
+	 *
+	 * This function is called in response to PHP's rename().
+	 *
+	 * @param string $from_uri
+	 *   the URI to the current file.
+	 * @param string $to_uri
+	 *   the URI which $from_uri should be renamed to.
+	 * @return bool
+	 *   true on success or false on failure.
+	 *
+	 * @package Stream Wrappers
+	 * @see WP_Stream_Wrapper_Interface::rename()
+	 * @see rename()
+	 * @link http://php.net/manual/en/streamwrapper.rename.php
+	 * @since 1.0.0
+	 */
+	public function rename($from_uri, $to_uri) {
+		return rename($this->get_local_path($from_uri), $this->get_local_path($to_uri));
+ }
 
 	/**
 	 * Implements WP_Stream_Wrapper_Interface::stream_open()
