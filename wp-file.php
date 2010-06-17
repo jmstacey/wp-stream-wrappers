e6a926de49964a2995e351e60af724ee8857542d
Jon Stacey <jon@jonsview.com>
2010-06-17 11:42:45 -0500

    Initial import of wp-file.php which contains the new WordPress File API.

---
+++ wp-file.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * This file contains the WordPress File API that is compatible with stream
+ * wrappers. WP_File could also be known as WP_Stream (if that makes more
+ * sense). A WP_File is a stream. A stream is a resource object which
+ * exhibits streamable behavior.
+ *
+ * @link http://www.php.net/manual/en/intro.stream.php
+ *
+ * @package Stream Wrappers
+ */
+
+/**
+ * WordPress File API
+ *
+ * @todo add more thourough description here
+ *
+ * @package    Stream Wrappers
+ * @author     Jon Stacey <jon@jonsview.com>
+ * @version    Release: 1.0.0
+ * @link       http://www.php.net/manual/en/intro.stream.php
+ * @see        
+ * @since      Class available since Release 1.0.0
+ */
+class WP_File {
+
+
+
+}
+	
+?>
