717370ac5084b22a692bfab5069d9f6f27ffbcf0
jmstacey <jmstacey@f69abe3c-2ee2-4550-9a6d-17bd458115be>
2010-06-17 20:46:00 +0000

    Add version number to local stream wrapper. Added registration functions for the local stream wrapper.
    git-svn-id: https://gsoc.svn.wordpress.org/2010/jmstacey/wp-stream-wrappers/trunk@349 f69abe3c-2ee2-4550-9a6d-17bd458115be

--- wp-local-stream-wrapper.php
+++ wp-local-stream-wrapper.php
@@ -10,6 +10,34 @@
  * @package Stream Wrappers
  */
 
+define('WP_LOCAL_STREAM_WRAPPER_VERSION', '1.0.0');
+
+/**
+ * Registers the WP Local Stream Wrapper
+ * 
+ * Prepares the WordPress Local Stream Wrapper for use
+ *
+ * @package Stream Wrappers
+ * @since 1.0.0
+ */
+function wp_local_stream_wrapper_register() {
+	$scheme = 'local'; // Wrapper scheme
+	
+	// Wrapper registration metadata
+	$wrapper_metadata = array(
+		'name' => 'WP Local Stream Wrapper',
+		'class' => 'WP_Local_Stream_Wrapper',
+		'description' => 'WP Local Stream Wrapper provides a ready to use base for interacting with files on the local filesystem.'
+	);
+
+	// Register this wrapper
+	WP_Stream_Wrapper_Registry::register_wrapper($scheme, $wrapper_metadata);
+}
+
+// Register test stream wrapper
+add_action('register_stream_wrapper', 'wp_local_stream_wrapper_register', 1, 0);
+
+
 /**
  * WordPress Local Stream Wrapper
  *
