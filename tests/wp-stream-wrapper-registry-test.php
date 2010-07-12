9f60d8ceb43c06c5cd23b20484bec4f5f929cd15
Jon Stacey <jon@jonsview.com>
2010-07-12 14:08:19 -0500

    Added basic files for upcoming PHPUnit tests. New phpunit.xml with the wp-stream-wrapper-registry-test.php file listed. The wp-stream-wrapper-registry-test.php has simple placeholders for the things that need to be tested in this class.

---
+++ tests/wp-stream-wrapper-registry-test.php
@@ -0,0 +1,39 @@
+<?php
+
+require_once 'PHPUnit/Framework.php';
+
+class WP_Stream_Wrapper_Registry_Test extends PHPUnit_Framework_TestCase {
+	public function setUp() {
+		print __METHOD__ . "\n";
+	}
+	
+	public function tearDown() {
+		print __METHOD__ . "\n";
+	}
+	
+	public function test_register_wrapper() {
+		// @todo implement
+		print __METHOD__ . "\n";
+	}
+	
+	public function test_unregister_wrapper() {
+		// @todo implement
+		print __METHOD__ . "\n";
+	}
+	
+	public function test_get_stream_wrappers() {
+		// @todo implement
+		print __METHOD__ . "\n";
+	}
+	
+	public function test_get_registry() {
+		// @todo implement
+		print __METHOD__ . "\n";
+	}
+	
+	public function test_clone() {
+		
+	}
+}
+
+?>
