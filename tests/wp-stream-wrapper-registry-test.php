28a003e72350e093d8b6bcb0c0de1ac10adeb0df
Jon Stacey <jon@jonsview.com>
2010-07-12 16:43:29 -0500

    Updated require to make sure that the stream wrappers plugin is initialized in the proper order. Removed unnecessary setUp and tearDown calls. Marked tests incomplete that still need to be implemented.

--- tests/wp-stream-wrapper-registry-test.php
+++ tests/wp-stream-wrapper-registry-test.php
@@ -1,38 +1,39 @@
 <?php
 
-require_once 'PHPUnit/Framework.php';
+// Make sure the stream wrappers plugin is initialized in the proper sequence.
+require_once WP_PLUGIN_DIR.'/wp-stream-wrappers/wp-stream-wrappers.php';
 
-class WP_Stream_Wrapper_Registry_Test extends PHPUnit_Framework_TestCase {
-	public function setUp() {
-		print __METHOD__ . "\n";
+class WP_Stream_Wrapper_Registry_Test extends WPTestCase {
+
+	public function test_class_attributes() {
+		$this->assertClassHasStaticAttribute('stream_wrappers', 'WP_Stream_Wrapper_Registry');
+		$this->assertClassHasStaticAttribute('registry', 'WP_Stream_Wrapper_Registry');
 	}
 	
-	public function tearDown() {
-		print __METHOD__ . "\n";
+	public function test_get_registry() {
+		$registry = WP_Stream_Wrapper_Registry::get_registry();
+		
+		$this->assertTrue(is_object($registry), "get_registry() should return an object.");
 	}
 	
 	public function test_register_wrapper() {
 		// @todo implement
-		print __METHOD__ . "\n";
+		$this->markTestIncomplete("This test has not been implemented yet");
 	}
 	
 	public function test_unregister_wrapper() {
 		// @todo implement
-		print __METHOD__ . "\n";
+		$this->markTestIncomplete("This test has not been implemented yet");
 	}
 	
 	public function test_get_stream_wrappers() {
 		// @todo implement
-		print __METHOD__ . "\n";
-	}
-	
-	public function test_get_registry() {
-		// @todo implement
-		print __METHOD__ . "\n";
+		$this->markTestIncomplete("This test has not been implemented yet");
 	}
 	
 	public function test_clone() {
-		
+		// @todo implement
+		$this->markTestIncomplete("This test has not been implemented yet");
 	}
 }
 
