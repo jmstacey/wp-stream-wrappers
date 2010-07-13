f578363f5f37acf78067e22e66fc8b3de13eb9ab
Jon Stacey <jon@jonsview.com>
2010-07-13 16:56:18 -0500

    End of day commit.

--- tests/wp-local-stream-wrapper-base-test.php
+++ tests/wp-local-stream-wrapper-base-test.php
@@ -10,15 +10,22 @@
  */
 require_once WP_PLUGIN_DIR.'/wp-stream-wrappers/wp-stream-wrappers.php';
 
+/** 
+ * This file contains the WP Test Stream Wrapper class.
+ */
+require_once WP_PLUGIN_DIR.'/wp-stream-wrappers/tests/wp-test-stream-wrapper.php';
+
 /**
  * WordPress Local Stream Wrapper Base Test cases
  *
- * This class provides tests for the WP Local Stream Wrapper Base.
+ * This class provides tests for the WP Local Stream Wrapper Base class.
+ * The test stream wrapper is used to perform tests which builds on this
+ * base class.
  *
  * @package    Stream Wrappers
  * @author     Jon Stacey <jon@jonsview.com>
  * @version    Release: 1.0.0
- * @see        WP_Local_Stream_Wrapper
+ * @see        WP_Local_Stream_Wrapper_Base
  * @since      Class available since Release 1.0.0
  */
 class WP_Local_Stream_Wrapper_Base_Test extends WPTestCase {
@@ -27,28 +34,42 @@ class WP_Local_Stream_Wrapper_Base_Test extends WPTestCase {
 	 * Setup this test case
 	 */
 	function setUp() {
-		// @todo does anything need to be done here?
+		// Register test stream wrapper
+		wp_test_stream_wrapper_register();
 	}
 	
 	/**
-	 * Tests getting the scheme of a stream
+	 * The following things should be tested in this first round:
 	 *
-	 * Tests WP_Stream::scheme()
+	 * - mkdir()
+	 * - rmdir()
+	 * - creating a file
+	 * - Writing to a file
+	 * - unlinking (deleting) a file
+	 * - wp_chmod()
+	 * - wp_realpath()
+	 * - wp_tempnam_stream_compatible()
+	 * - wp_dirname()
+	 * - get_local_path()
+	 * - get_wrapper_path()
 	 */
-	public function test_scheme() {
-		$stream   = 'test://example/path1/path2/hello_world.txt';
-		$expected = 'test';
-		
-		$actual = WP_Stream::scheme($stream);
-		
-		$this->assertEquals($expected, $actual, "WP_Stream::scheme returned '$actual' instead of the expected '$expected'.");
+	
+	/**
+	 * Tests creating directories
+	 *
+	 * Tests WP_Local_Stream_Wrapper_Base::mkdir()
+	 */
+	public function test_mkdir() {
+		$this->markTestIncomplete('This test has not been implemented yet.');
 	}
 	
-	
-	
-	
-	$this->markTestIncomplete('This test has not been implemented yet.');
-	
+	/**
+	 * Teardown this test case
+	 */
+	function tearDown() {
+		// Unregister test stream wrapper
+		WP_Stream_Wrapper_Registry::unregister_wrapper('test');
+	}
 }
 
 ?>
