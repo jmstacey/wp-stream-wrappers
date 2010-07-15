8bfbd02e873c6da76a415d08c39d95e5b52d335e
Jon Stacey <jon@jonsview.com>
2010-07-15 17:20:36 -0500

    Added some tests for wp_chmod() and wp_realpath().

--- tests/wp-file-api-helpers-test.php
+++ tests/wp-file-api-helpers-test.php
@@ -29,13 +29,90 @@ require_once WP_PLUGIN_DIR.'/wp-stream-wrappers/tests/wp-test-stream-wrapper.php
 class WP_File_Helpers_Test extends WPTestCase {
 	
 	/**
+	 * Test file name
+	 *
+	 * This variable holds the name fo the test file
+	 *
+	 * @var string
+	 * @access private
+	 */
+	private $filename;
+	
+	/**
+	 * Test file URI
+	 *
+	 * This variable holds the URI of the test file
+	 *
+	 * @var string
+	 * @access private
+	 */
+	private $uri;
+	
+	/**
+	 * Test file path
+	 *
+	 * This variable holds the local path that the URI resides at.
+	 *
+	 * @var string
+	 * @access private
+	 */
+	private $path;
+	
+	/**
+	 * Test file contents
+	 *
+	 * This variable holds the expected contents of the test file.
+	 *
+	 * @var string
+	 * @access private
+	 */
+	private $expected_contents;
+	
+	/**
+	 * Test directory path
+	 *
+	 * This variable holds the local path that the test wrapper is pointed to.
+	 *
+	 * @var string
+	 * @access private
+	 */
+	private $test_dir;
+	
+	/**
 	 * Setup this test case
 	 */
 	function setUp() {
 		// Register test stream wrapper
 		wp_test_stream_wrapper_register();
 		
-		// @todo create a test directory with some example files and directories
+		/**
+		 * Initialize instance variables
+		 */
+		$wrapper 		= WP_Stream::new_wrapper_instance('test://');
+		$this->test_dir = $wrapper->get_wrapper_path();
+		
+		$this->filename = 'testfile.txt';
+		$this->uri		= 'test://'.$this->filename;
+		$this->path		= $this->test_dir.'/'.$this->filename;
+		
+		$this->expected_contents = 'The miracle is this - the more we share, the more we have. -- Leonard Nimoy';
+		
+		/**
+		 * Setup and assert starting evironment
+		 */
+		if (file_exists($this->uri)) {
+			unlink($this->uri);
+		}
+		
+		$this->assertFileNotExists($this->uri);
+		$this->assertFileNotExists($this->path);
+		
+		// Open and write to the file
+		$fh = fopen($this->uri, 'w');
+		fwrite($fh, $this->expected_contents);
+		fclose($fh);
+		$this->assertFileExists($this->path);
+		$this->assertEquals(filesize($this->uri), filesize($this->path));
 	}
 	
 	/**
@@ -53,17 +130,59 @@ class WP_File_Helpers_Test extends WPTestCase {
 	 * Tests wp_chmod()
 	 */
 	public function test_wp_chmod() {
-		$this->markTestIncomplete('This test has not been implemented yet.');
+		$perms 		 = fileperms($this->uri);
+		$perms_octal = substr(sprintf('%o', $perms), -4);
+		
+		$this->assertType('int', $perms);
+		
+		$this->assertEquals($perms, fileperms($this->path));
+		
+		$this->assertTrue(wp_chmod($this->uri, 0000));
+		clearstatcache();
+		$this->assertNotEquals($perms, fileperms($this->uri));
+		
+		$cur_octal = substr(sprintf('%o', fileperms($this->uri)), -4);
+		$this->assertEquals(0000, $cur_octal);
+		$this->assertEquals(fileperms($this->uri), fileperms($this->path));
+		
+		$this->assertTrue(wp_chmod($this->uri, $perms));
+		clearstatcache();
+		$cur_octal = substr(sprintf('%o', fileperms($this->uri)), -4);
+		$this->assertEquals($perms_octal, $cur_octal);
+		$this->assertEquals(fileperms($this->uri), fileperms($this->path));
+	}
+	
+	/**
+	 * Tests getting cononical [absolute] path of file
+	 *
+	 * Tests wp_realpath()
+	 */
+	public function test_wp_realpath() {
+		/**
+		 * Test using URI
+		 */
+		$this->assertEquals($this->path, wp_realpath($this->uri));
+		$this->assertFalse(wp_realpath('test://somedir/../'.$this->filename));
+		
+		// @todo touch() isn't working. Probably has to do with realpath()
+		// wrapper implementation when file doesn't exist.
+		
+		/**
+		 * Test using path
+		 */
+		$this->assertEquals($this->path, wp_realpath($this->path));
+		$this->assertEquals($this->path, wp_realpath($this->test_dir.'/../stream_tests/'.$this->filename));
 	}
 	
 	/**
 	 * Teardown this test case
 	 */
 	function tearDown() {
+		unlink($this->uri);
+		$this->assertFileNotExists($this->uri);
+		
 		// Unregister test stream wrapper
 		WP_Stream_Wrapper_Registry::unregister_wrapper('test');
-		
-		// @todo delete the test directory
 	}
 }
 
