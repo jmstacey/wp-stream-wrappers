7be6de4fc015ae91bc2a4f194ac1cbf608f64b8a
Jon Stacey <jon@jonsview.com>
2010-07-28 17:00:33 -0500

    Added rudimentary test of the local stream wrapper get_wrapper_path() method.

--- tests/wp-local-stream-wrapper-base-test.php
+++ tests/wp-local-stream-wrapper-base-test.php
@@ -15,6 +15,11 @@ require_once WP_PLUGIN_DIR.'/wp-stream-wrappers/wp-stream-wrappers.php';
  */
 require_once WP_PLUGIN_DIR.'/wp-stream-wrappers/tests/wp-test-stream-wrapper.php';
 
+/** 
+ * This file contains the WP Local Stream Wrapper class.
+ */
+require_once WP_PLUGIN_DIR.'/wp-stream-wrappers/wp-local-stream-wrapper/wp-local-stream-wrapper.php';
+
 /**
  * WordPress Local Stream Wrapper Base Test cases
  *
@@ -46,6 +51,7 @@ class WP_Local_Stream_Wrapper_Base_Test extends WPTestCase {
 	function setUp() {
 		// Register test stream wrapper
 		wp_test_stream_wrapper_register();
+		wp_local_stream_wrapper_register();
 		
 		// Set the expected test directory
 		$wrapper = WP_Stream::new_wrapper_instance('test://');
@@ -201,6 +207,14 @@ class WP_Local_Stream_Wrapper_Base_Test extends WPTestCase {
 		// At the moment this simply simulates the plugins loaded action
 		// so that the local wraper code coverage is counted
 		do_action('plugins_loaded');
+	}
+	
+	/**
+	 * Tests WP_Local_Stream_Wrapper->get_wrapper_path()
+	 */
+	public function test_wp_local_stream_wrapper_get_wrapper_path() {
+		$actual = WP_Stream::new_wrapper_instance('local://')->get_wrapper_path();
+		$this->assertEquals(WP_CONTENT_DIR, $actual);
 	}
 	
 	/**
